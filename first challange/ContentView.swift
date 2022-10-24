3bda912f90cc64650c43ce475ad511152a859a0b
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-10-24 15:28:07 +0200

    Update

--- first challange/ContentView.swift
+++ first challange/ContentView.swift
@@ -20,7 +20,7 @@ struct ContentView: View {
                 Image(systemName: "globe")
                     .imageScale(.large)
                     .foregroundColor(.accentColor)
-                Text("Hello, world!")
+                Text("Eldorbek")
             }
         }
         .padding()
