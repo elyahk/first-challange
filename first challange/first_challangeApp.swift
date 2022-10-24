46c981be7fdb209199536fbab0ae8fc797ba5ccf
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-10-24 15:02:47 +0200

    Initial Commit

---
+++ first challange/first_challangeApp.swift
@@ -0,0 +1,17 @@
+//
+//  first_challangeApp.swift
+//  first challange
+//
+//  Created by Eldorbek Nusratov on 24/10/22.
+//
+
+import SwiftUI
+
+@main
+struct first_challangeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
