ef0d0acba3b034c46ed5af672e23f42aed5b502c
cirosec99 <secondulfociro99@gmail.com>
2022-10-25 15:54:53 +0200

    i added the icons we need

---
+++ first challange/Images.swift
@@ -0,0 +1,21 @@
+//
+//  Images.swift
+//  first challange
+//
+//  Created by Ciro Secondulfo on 25/10/22.
+//
+
+import Foundation
+extension String {
+    
+    static let education = "custom.graduationcap.fill"
+    static let mainpageicon = "wallet.pass.fill"
+    static let entertainment = "custom.gamecontroller.fill"
+    static let clothes = "tshirt.fill"
+    static let shopping = "suitcase.cart.fill"
+    static let house = "house.fill"
+    static let history = "clock.fill"
+    static let other = "line.3.horizontal.circle.fill"
+    static let topviewIncomes = "custom.square.and.arrow.down.on.square.fill"
+    static let topviewExpences = "custom.square.and.arrow.up.on.square.fill"
+}
