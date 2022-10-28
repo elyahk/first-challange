4c0e65078c11323ee63b025236de383473ac8e7a
yahyamomtaz <yahyamomtaz@gmail.com>
2022-10-28 15:42:57 +0200

    add target page

---
+++ TargetView.swift
@@ -0,0 +1,23 @@
+//
+//  TargetView.swift
+//  first challange
+//
+//  Created by Yahya Momtaz on 28/10/22.
+//
+
+import SwiftUI
+
+struct TargetView: View {
+    var body: some View {
+//        enter amount view
+//ttile
+//        navigation bar
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct TargetView_Previews: PreviewProvider {
+    static var previews: some View {
+        TargetView()
+    }
+}
