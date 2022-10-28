bcebdc8cc8c44538cc90926cb4da9ef3a74dfa4a
yahyamomtaz <yahyamomtaz@gmail.com>
2022-10-28 16:05:53 +0200

    I completed the Target page

--- TargetView.swift
+++ TargetView.swift
@@ -8,11 +8,42 @@
 import SwiftUI
 
 struct TargetView: View {
+    @Environment(\.presentationMode) var presentationMode
+    @State var segmentedChoice = 0
     var body: some View {
+        
 //        enter amount view
-//ttile
+//          ttile
 //        navigation bar
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+//        transaction main view
+        NavigationStack {
+            VStack(alignment: .leading) {
+                EnterAmountView(shouldShowTitle: false, title: "", amount: "")
+                    .padding([.top], 10)
+                    
+                Spacer()
+            }
+            .background(Color(hex: .gray_light))
+            .navigationTitle("Enter your Target")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .foregroundColor(Color.red)
+                }
+            }
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        presentationMode.wrappedValue.dismiss()
+                    }
+                    .foregroundColor(Color.blue)
+                }
+            }
+            .toolbarBackground(.visible, for: .navigationBar)
+        }
     }
 }
 
