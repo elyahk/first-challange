5ba68252a934aecd176eb6873c9dec290c45fde8
elyahk-apple-academy <enusratov22@fed.idserve.net>
2022-10-28 14:40:41 +0200

    Remove unnecessary file

--- first challange/Views/DashboardTopView.swift
+++ first challange/Views/DashboardTopView.swift
@@ -55,53 +55,6 @@ struct DashboardTopView2: View {
     }
 }
 
-
-
-struct DashboardTopView: View {
-    var body: some View {
-        VStack {
-            ZStack {
-                RoundedRectangle(cornerRadius: 25)
-                    .fill(Color(red: 0.7490196078431373, green: 0.8627450980392157, blue: 0.6823529411764706))
-                    .frame(
-                        width: screenWidth,
-                        height: universalHeight(height: 450)
-                    )
-                    .offset(y: -universalHeight(height: 100))
-                
-                VStack {
-                    VStack {
-                        Text("You are reaching your target")
-                            .font(.system(size: 25, weight: .light))
-                        Text("500 $")
-                            .font(.system(size: 25, weight: .light))
-                            .fontWeight(.black)
-                    }
-                    .padding(.top, 40)
-                    DashboardTopSummaryView()
-                        .frame(height: 60)
-                        .padding(.top, 20)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
-                    
-                    DashboardTopSummaryView()
-                        .padding(.top, 40)
-                        .frame(height: 60)
-                        .padding(.leading, 50)
-                        .padding(.trailing, 50)
-                    
-                    Spacer()
-                }
-            }
-            Spacer()
-            
-            
-            
-        }
-        
-    }
-}
-
 struct DashboardTopView_Previews: PreviewProvider {
     static var previews: some View {
         Group {
